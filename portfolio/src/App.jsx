ca0e0a245a5852868ec956eccf541c43ce8d2d65
Caspian Thackeray <cthackt@NotATrashCan.local>
2023-07-18 13:42:27 -0700

    tentative ui idea but might be too boring

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+   return (
+      <div className="App">
+         <div className="display"></div>
+         <div className='nav'>
+            <ul>
+               <li>Home</li>
+               <li>Projects</li>
+               <li>About</li>
+            </ul>
+         </div>
+      </div>
+   );
+}
+
+export default App;
